64ba65bff006dda83fa95daaf134df7fb440ec9e
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-10-31 01:14:45 +0700

    Tailwind CSS & Vite Hot Reloading

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ?? 'Workopia | Find and list jobs' }}</title>
+    @vite('resources/css/app.css')
 </head>
 
 <body class="bg-gray-100">
