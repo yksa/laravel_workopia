27681c73ee10c8366fde9e3d0b05c084b7d1a541
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-10-30 20:52:31 +0700

    Create & Display Views

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    return view('jobs.index');
+})->name('jobs');
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+})->name('jobs.create');
