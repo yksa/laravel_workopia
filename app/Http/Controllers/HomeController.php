830c865b592af3aad48a1c3c928e4de7fb85bbcd
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-11-03 21:38:06 +0700

    Homepage Jobs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Job;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(): View
     {
-        return view('pages.index');
+        $jobs = Job::latest()->limit(6)->get();
+
+        return view('pages.index')->with('jobs', $jobs);
     }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,18 @@
 <x-layout>
-    <h1>Welcome To Workopia</h1>
+    <h2 class="mb-4 border-gray-300 p-3 text-center text-3xl font-bold">Welcome To Workopia</h2>
+
+    <div class="mb-6 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        @if (count($jobs) === 0)
+            <p>No jobs available</p>
+        @else
+            @foreach ($jobs as $job)
+                <x-job-card :job="$job" />
+            @endforeach
+        @endif
+    </div>
+    <a href="{{ route('jobs.index') }}" class="block text-center text-xl">
+        <i class="fa fa-arrow-alt-circle-right"></i> Show All Jobs
+    </a>
 
     <x-bottom-banner />
 </x-layout>
