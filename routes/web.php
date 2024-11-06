f448b49e81ef5a30b077415378e3788490e72fad
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-11-07 00:10:59 +0700

    Middleware Overview

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,14 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Middleware\LogRequest;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::resource('jobs', JobController::class);
 
-Route::get('register', [RegisterController::class, 'register'])->name('register');
+Route::get('register', [RegisterController::class, 'register'])->name('register')->middleware(LogRequest::class);
 Route::post('register', [RegisterController::class, 'store'])->name('register.store');
-Route::get('login', [LoginController::class, 'login'])->name('login');
+Route::get('login', [LoginController::class, 'login'])->name('login')->middleware(LogRequest::class);
 Route::post('login', [LoginController::class, 'authenticate'])->name('login.authenticate');
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
