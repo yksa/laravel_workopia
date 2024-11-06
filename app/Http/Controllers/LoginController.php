0c0d20064581b5d81aa5b89122409969f4adc624
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-11-06 22:34:20 +0700

    Login User

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -16,5 +17,23 @@ class LoginController extends Controller
     public function login(): View
     {
         return view('auth.login');
+    }
+
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->route('home')->with('success', 'Login successful!');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,24 @@
 <x-layout>
     <x-slot name="title">Login</x-slot>
-    <h1>Login</h1>
+    <div class="mx-auto mt-12 w-full rounded-lg bg-white p-8 py-12 shadow-md md:max-w-xl">
+        <h2 class="mb-4 text-center text-4xl font-bold">
+            Login
+        </h2>
+
+        <form action="{{ route('login.authenticate') }}" method="POST">
+            @csrf
+            <x-inputs.text type="email" id="email" name="email" placeholder="Email address" />
+            <x-inputs.text type="password" id="password" name="password" placeholder="Password" />
+
+            <button type="submit"
+                class="my-3 w-full rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:outline-none">
+                Login
+            </button>
+
+            <p class="mt-4 text-gray-500">
+                Don't have an account?
+                <a class="text-blue-900" href="{{ route('register') }}">Register</a>
+            </p>
+        </form>
+    </div>
 </x-layout>
