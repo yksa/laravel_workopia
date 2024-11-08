04b421f08d2329911d39fd5d787ad5b129c99c05
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-11-08 14:58:10 +0700

    Seeding Bookmarks

--- database/seeders/TestUserSeeder.php
+++ database/seeders/TestUserSeeder.php
@@ -14,14 +14,16 @@ class TestUserSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        User::create([
+        $user = User::create([
             'name' => 'Test User',
             'email' => 'test@test.com',
             'email_verified_at' => Carbon::now(),
             'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
         ]);
+
+        return $user;
     }
 }
