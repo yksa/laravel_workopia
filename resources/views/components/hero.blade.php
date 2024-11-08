24e2dc66e68957adb5168dcc793555fe6d426150
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-11-08 20:43:24 +0700

    Search Component & Route

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -4,14 +4,6 @@
         <h2 class="mb-8 text-4xl font-bold text-white md:text-5xl">
             Find Your Dream Job
         </h2>
-        <form class="mx-5 block space-y-2 md:mx-auto md:space-x-2">
-            <input type="text" name="keywords" placeholder="Keywords"
-                class="w-full px-4 py-3 focus:outline-none md:w-72" />
-            <input type="text" name="location" placeholder="Location"
-                class="w-full px-4 py-3 focus:outline-none md:w-72" />
-            <button class="w-full bg-blue-700 px-4 py-3 text-white hover:bg-blue-600 focus:outline-none md:w-auto">
-                <i class="fa fa-search mr-1"></i> Search
-            </button>
-        </form>
+        <x-search />
     </div>
 </section>
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,4 +1,7 @@
 <x-layout>
+    <div class="mb-4 flex h-24 items-center justify-center rounded bg-blue-900 px-4">
+        <x-search />
+    </div>
     <div class="mb-6 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         @if (count($jobs) === 0)
             <p>No jobs available</p>
